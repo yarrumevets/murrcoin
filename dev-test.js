61bb43d84a761f14b0c1f337c92b73c79bdf45fc
Steven Murray <steve@Stevens-MacBook-Pro.local>
2021-06-19 23:18:16 -0700

    Moved block and blockchain, and their tests inside new blockchain folder. Removed dev-test.

--- dev-test.js
+++
@@ -1,8 +0,0 @@
-const Block = require("./block");
-
-// const block = new Block("foo", "bar", "boo", "baz");
-// console.log(block.toString());
-// console.log(Block.genesis().toString());
-
-const fooBlock = Block.mineBlock(Block.genesis(), "foo");
-console.log(fooBlock.toString());
