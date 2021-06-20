6c0f7949c78311daae659ee9fb77109aeca9f0ed
Steven Murray <steve@Stevens-MacBook-Pro.local>
2021-06-20 09:53:28 -0700

    Added mining to the api, bodyparser node module

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Blockchain = require("../blockchain");
+const bodyParser = require("body-parser");
 
 // HTTP_PORT=1234 npm run dev
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
@@ -7,8 +8,16 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const bc = new Blockchain();
 
+app.use(bodyParser.json()); // creates a body obj in the request.
+
 app.get("/blocks", (req, res) => {
   res.json(bc.chain);
+});
+
+app.post("/mine", (req, res) => {
+  const block = bc.addBlock(req.body.data);
+  console.log(`Block inserted: ${block.toString()}`);
+  res.redirect("/blocks");
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
